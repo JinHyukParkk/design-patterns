d3654e3e5873595f6de764aa72aaec495ef04362
hyuk <pjh08190819@naver.com>
2021-08-29 21:53:35 +0900

    study proxy pattern

---
+++ src/proxy/Html.java
@@ -0,0 +1,10 @@
+package proxy;
+
+public class Html {
+
+    private String url;
+
+    public Html(String url) {
+        this.url = url;
+    }
+}
---
+++ src/proxy/Main.java
@@ -0,0 +1,17 @@
+package proxy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+
+
+        IBrowser iBrowser = new BrowserProxy("www.naver.com");
+        iBrowser.show();
+        iBrowser.show();
+        iBrowser.show();
+    }
+}
