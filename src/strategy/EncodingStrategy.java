daabfb089c5b94c86342ee9c0e6cf0f0b6fe25fa
hyuk <pjh08190819@naver.com>
2021-09-10 23:16:48 +0900

    study strategy pattern

---
+++ src/strategy/EncodingStrategy.java
@@ -0,0 +1,6 @@
+package strategy;
+
+public interface EncodingStrategy {
+
+    String encode(String text);
+}
---
+++ src/strategy/Main.java
@@ -0,0 +1,32 @@
+package strategy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Encoder encoder = new Encoder();
+
+        // base64
+        EncodingStrategy base64 = new Base64Strategy();
+
+        // normal
+        EncodingStrategy normal = new NormalStrategy();
+
+        // append
+        EncodingStrategy append = new AppendStrategy();
+
+        String message = "hello java";
+
+        encoder.setEncodingStrategy(base64);
+        String base64Result = encoder.getMessage(message);
+        System.out.println(base64Result);
+
+        encoder.setEncodingStrategy(normal);
+        String normalResult = encoder.getMessage(message);
+        System.out.println(normalResult);
+
+        encoder.setEncodingStrategy(append);
+        String appendResult = encoder.getMessage(message);
+        System.out.println(appendResult);
+
+    }
+}
